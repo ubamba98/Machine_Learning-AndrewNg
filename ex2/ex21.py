40e955657af54c4b890ff84acc35a175636439bf
ubamba98 <34593214+ubamba98@users.noreply.github.com>
2017-12-22 13:30:45 +0530

    Update ex21.py

--- ex2/ex21.py
+++ ex2/ex21.py
@@ -1,6 +1,4 @@
-import pandas as pd
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 
 data = np.genfromtxt('ex2data1.txt', delimiter=',')
@@ -23,9 +21,11 @@ plt.xlabel('Exam 1 Score')
 plt.ylabel('Exam 2 Score')
 plt.title('ScatterData')
 plt.show()
+
 y = np.array([temp])
+
 def sigmoid(z):
-    return 1.0/(1.0+math.e**(-1*z))
+    return 1.0/(1.0+np.e**(-1*z))
 
 
 theta = np.random.rand(X.shape[1],1)
@@ -35,9 +35,10 @@ m = len(y)
 def grad(x):
     h = sigmoid(np.dot(X,theta))
     return 1/m * np.dot(x.T,(h-y.T))
+
 alpha = 0.0001
 illust =25000
-
+print(grad(X))
 for i in range(illust):
     t = grad(X)
     theta[0,:] = theta[0,:] - alpha * t[0,:]
